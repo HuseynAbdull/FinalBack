8b75d9790d4bb69fb1d1b0e95fab0e303c0b8bc3
HuseynAbdull <Huseynka@code.edu.az>
2023-05-09 16:36:00 +0300

    Footer Pages

---
+++ FinalProjectCode/Controllers/FooterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectCode.Controllers
+{
+    public class FooterController : Controller
+    {
+       
+        public async Task<IActionResult> Shipping()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AboutUs()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ContactUs()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Terms()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> California()
+        {
+            return View();
+        }
+
+    }
+}
